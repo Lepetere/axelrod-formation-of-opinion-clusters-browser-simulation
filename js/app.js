98d109c0b67a11a4783932ca592843b19e58886c
Peter Fessel <peter.fessel@live.de>
2015-12-22 21:00:57 +0100

    initialize a 10*10 grid of squares with a random tone of red plus a start button

--- js/app.js
+++ js/app.js
@@ -3,8 +3,21 @@ var APP = APP || {};
 APP.UI = (function () {
 
   var init = function () {
-    console.log("init");
+    $('#start-button').click(initGrid);
+    initGrid();
   };
+
+  function initGrid () {
+    $('#main-grid-container').empty();
+
+    for ( var i = 0; i < 100; i ++ ) {
+      var newGridCell = $('<div class="grid-cell"></div>'),
+        randomNumber = Math.floor(Math.random() * 9),
+        color = '#F' + randomNumber + randomNumber;
+      $(newGridCell).css('background-color', color);
+      $('#main-grid-container').append(newGridCell);
+    }
+  }
 	
   var module = {};
   module.init = init;
