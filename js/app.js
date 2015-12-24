db1aab3041b631ec15d6cb6faf603ccf718d6d00
Peter Fessel <peter.fessel@live.de>
2015-12-24 15:55:52 +0100

    randomize traits and generate tones of red for them, make displayed feature switchable

--- js/app.js
+++ js/app.js
@@ -7,7 +7,9 @@ APP.UI = (function () {
     VERTICAL_GRID_DIMENSION = 10,
     NUMBER_OF_GRID_CELLS = HORIZONTAL_GRID_DIMENSION * VERTICAL_GRID_DIMENSION,
     SVG_WIDTH, // set in 'init'
-    SVG_HEIGHT; // set in 'init'
+    SVG_HEIGHT,
+    NUMBER_OF_OPINION_DIMENSIONS = 2, // the number of features that each cell has
+    NUMBER_OF_TRAITS = 5; // the number of traits 
 
   var grid;
 
@@ -15,16 +17,34 @@ APP.UI = (function () {
     displayName: "GridCell",
 
     statics: {
-      // generates a random tone of red
-      generateColor: function () {
-        var randomNumber = Math.floor(Math.random() * 9);
-        return '#F' + randomNumber + randomNumber;
+      // generates a color to represent the trait of the currently selected feature
+      generateColor: function (showTrait, traits, showOpinionDimensionNumber) {
+        if (showTrait) {
+          var tones = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'],
+            traitTone = tones[Math.floor((traits[showOpinionDimensionNumber] / NUMBER_OF_TRAITS) * 16)];
+          return '#F' + traitTone + traitTone;
+        }
+        else {
+          // just return white for now for the similarity of neighbors // TO DO
+          return '#FFF';
+        }
       }
     },
 
     getInitialState: function () {
-      // TO DO: randomize initial state
-      return {};
+      var traits = [];
+      for ( var i = 0; i < NUMBER_OF_OPINION_DIMENSIONS; i ++ ) {
+        traits.push(Math.floor(Math.random() * NUMBER_OF_TRAITS));
+      }
+
+      return {
+        // initially show the state  of the first opinion dimension (the first feature's trait)
+        showOpinionDimensionNumber: 1,
+        // indicates if the trait of a particular feature or the similarity between neighbors is displayed
+        // set to true for the traits and to false for the similarity
+        showTrait: true,
+        traits: traits
+      };
     },
 
     shouldComponentUpdate: function () {
@@ -36,7 +56,7 @@ APP.UI = (function () {
 
       return React.DOM.rect({
         className: 'grid-cell',
-        fill: this.constructor.generateColor(),
+        fill: this.constructor.generateColor(this.state.showTrait, this.state.traits, this.state.showOpinionDimensionNumber),
         x: (SVG_WIDTH / HORIZONTAL_GRID_DIMENSION) * (position % HORIZONTAL_GRID_DIMENSION),
         y: (SVG_HEIGHT / VERTICAL_GRID_DIMENSION) * ((position - (position % VERTICAL_GRID_DIMENSION)) / VERTICAL_GRID_DIMENSION)
       });
